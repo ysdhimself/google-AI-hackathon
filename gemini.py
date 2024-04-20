9b2bb524492261af50175a2aba66e3af449bf480
Muhammad Hafiz Azhari <mhafiz.azh@gmail.com>
2024-04-20 11:16:13 -0600

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -3,16 +3,18 @@ from dotenv import load_dotenv
 import google.generativeai as genai
 import os
 
-
+# Function to load environment variables from .env file
 def configure():
     load_dotenv()
 
-
+# Main function to run the application
 def main():
+    # Load environment variables
     configure()
+    # Configure the generative AI with the API key from environment variables
     genai.configure(api_key=os.getenv('api_key'))
 
-    # Set up the model
+    # Set up the model configuration
     generation_config = {
         "temperature": 0.9,
         "top_p": 1,
@@ -20,6 +22,7 @@ def main():
         "max_output_tokens": 2048,
     }
 
+    # Set up the safety settings for the model
     safety_settings = [
         {
             "category": "HARM_CATEGORY_HARASSMENT",
@@ -39,15 +42,18 @@ def main():
         },
     ]
 
+    # Initialize the generative model with the specified configuration and safety settings
     model = genai.GenerativeModel(model_name="gemini-1.0-pro",
                                   generation_config=generation_config,
                                   safety_settings=safety_settings)
 
-    convo = model.start_chat(history=[
-    ])
+    # Start a chat with the model
+    convo = model.start_chat(history=[])
 
+    # Send a message to the model and print the model's response
     convo.send_message("tell me about canada")
     print(convo.last.text)
 
+# Run the main function if the script is run directly
 if __name__ == '__main__':
     main()
