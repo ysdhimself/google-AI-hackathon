56377c83087e5a8dc8af49f3e7397ffbf8f2f7a7
Muhammad Hafiz Azhari <mhafiz.azh@gmail.com>
2024-04-20 10:44:56 -0600

    Remove ignored files

---
+++ gemini.py
@@ -0,0 +1,53 @@
+import requests
+from dotenv import load_dotenv
+import google.generativeai as genai
+import os
+
+
+def configure():
+    load_dotenv()
+
+
+def main():
+    configure()
+    genai.configure(api_key=os.getenv('api_key'))
+
+    # Set up the model
+    generation_config = {
+        "temperature": 0.9,
+        "top_p": 1,
+        "top_k": 1,
+        "max_output_tokens": 2048,
+    }
+
+    safety_settings = [
+        {
+            "category": "HARM_CATEGORY_HARASSMENT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+        {
+            "category": "HARM_CATEGORY_HATE_SPEECH",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+        {
+            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+        {
+            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+        },
+    ]
+
+    model = genai.GenerativeModel(model_name="gemini-1.0-pro",
+                                  generation_config=generation_config,
+                                  safety_settings=safety_settings)
+
+    convo = model.start_chat(history=[
+    ])
+
+    convo.send_message("tell me about canada")
+    print(convo.last.text)
+
+if __name__ == '__main__':
+    main()
